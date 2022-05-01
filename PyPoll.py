fa3a424b8f8358a9393ff1b20fab8ce71374f406
Sam Caslowitz <sam.caslowitz@gmail.com>
2022-05-01 10:00:33 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,11 @@ candidate_votes = {}
 # Print the condidate name from each row
 candidate_options = []
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -50,5 +55,27 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     # 3. Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
-    # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+
+    # To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine the winning Candidate.
+    # 1. Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # 2. If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+         # 3. Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Vote Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+
+print(winning_candidate_summary)
